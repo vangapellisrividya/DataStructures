83bf8399531dce290dbf4d339daed80c863f354e
vangapellisrividya <vangapellisreevidya@gmail.com>
2021-11-26 22:18:58 +0530

    [srividya] Add: circulating list

---
+++ List/Circular.py
@@ -0,0 +1,17 @@
+''''
+    Created on Nov 26, 2021
+
+    @author:srividya
+    @Date:  26/11/2021
+    @Title: Circulary identical
+    
+    '''
+
+list1 = [10, 10, 0, 0, 10]
+list2 = [10, 10, 10, 0, 0]
+list3 = [1, 10, 10, 0, 0]
+
+print('Compare list1 and list2')
+print(' '.join(map(str, list2)) in ' '.join(map(str, list1 * 2)))
+print('Compare list1 and list3')
+print(' '.join(map(str, list3)) in ' '.join(map(str, list1 * 2)))
