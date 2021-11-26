f33e283338af2c01fdedab001f5d803c789c65fb
vangapellisrividya <vangapellisreevidya@gmail.com>
2021-11-26 22:29:00 +0530

    [srividya] Add: multiplies lists

---
+++ List/multiply_list.py
@@ -0,0 +1,14 @@
+''''
+    Created on Nov 26, 2021
+
+    @author:srividya
+    @Date:  26/11/2021
+    @Title: Multiply list
+    
+    '''
+
+list=[1,2,-8]
+tot = 1
+for x in list:
+    tot *= x
+print("mul output: ",tot)
