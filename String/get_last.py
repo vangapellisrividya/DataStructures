70e9b9c13a826724cc5a6f3cb484b6c6250de68a
vangapellisrividya <vangapellisreevidya@gmail.com>
2021-11-26 22:47:24 +0530

    [srividya] Add: get last  character

---
+++ String/get_last.py
@@ -0,0 +1,12 @@
+''''
+    Created on Nov 26, 2021
+
+    @author:srividya
+    @Date:  26/11/2021
+    @Title: fet lastt part of string
+    
+    '''
+
+str1 = 'https://www.w3resource.com/python-exercises/string'
+print(str1.rsplit('/', 1)[0])
+print(str1.rsplit('-', 1)[0])
