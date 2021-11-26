71a0941751431ec4c886dad57be46acd6811cb84
vangapellisrividya <vangapellisreevidya@gmail.com>
2021-11-26 22:48:50 +0530

    [srividya] Add: printing in upper and lowercase

---
+++ String/upper_lower.py
@@ -0,0 +1,12 @@
+''''
+    Created on Nov 26, 2021
+
+    @author:srividya
+    @Date:  26/11/2021
+    @Title: Upper and lower case
+    
+    '''
+
+user_input = input("What's your favourite language? ")
+print("My favourite language is ", user_input.upper())
+print("My favourite language is ", user_input.lower())
