850619b966763b97411aacd9e793f6208a459aac
vangapellisrividya <vangapellisreevidya@gmail.com>
2021-11-26 22:48:12 +0530

    [srividya] Add: printing in lowercase

---
+++ String/lowercase.py
@@ -0,0 +1,11 @@
+''''
+    Created on Nov 26, 2021
+
+    @author:srividya
+    @Date:  26/11/2021
+    @Title: lower case first n characters
+    
+    '''
+
+str1 = 'W3RESOURCE.COM'
+print(str1[:4].lower() + str1[4:])
