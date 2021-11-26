9420fe9a054f5c2c7c175318b75468280de8382a
vangapellisrividya <vangapellisreevidya@gmail.com>
2021-11-26 22:44:25 +0530

    [srividya] Add: longest word width

---
+++ String/longest_word.py
@@ -0,0 +1,18 @@
+''''
+    Created on Nov 26, 2021
+
+    @author:srividya
+    @Date:  26/11/2021
+    @Title: Longest word length
+    
+    '''
+
+def find_longest_word(words_list):
+    word_len = []
+    for n in words_list:
+        word_len.append((len(n), n))
+    word_len.sort()
+    return word_len[-1][0], word_len[-1][1]
+result = find_longest_word(["PHP", "Exercises", "Backend"])
+print("\nLongest word: ",result[1])
+print("Length of the longest word: ",result[0])
