3c9d921145e674f8f69f3deccd213f71bc7301cc
vangapellisrividya <vangapellisreevidya@gmail.com>
2021-11-26 22:36:33 +0530

    [srividya] Add: reverse of tuple

---
+++ tuple/reverse_tuple.py
@@ -0,0 +1,19 @@
+''''
+    Created on Nov 26, 2021
+
+    @author:srividya
+    @Date:  26/11/2021
+    @Title: slice
+    
+    '''
+
+#create a tuple
+x = ("w3resource")
+# Reversed the tuple
+y = reversed(x)
+print(tuple(y))
+#create another tuple
+x = (5, 10, 15, 20)
+# Reversed the tuple
+y = reversed(x)
+print(tuple(y))
