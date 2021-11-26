639a48639e09a352dacb0cc22e5aa5f4640f8baa
vangapellisrividya <vangapellisreevidya@gmail.com>
2021-11-26 22:34:32 +0530

    [srividya] Add: list in tuple

---
+++ tuple/list_tuple.py
@@ -0,0 +1,15 @@
+''''
+    Created on Nov 26, 2021
+
+    @author:srividya
+    @Date:  26/11/2021
+    @Title: list to tuple
+
+    '''
+
+sample_list = ['Compile', 'With', 'Favtutor']
+#convert list into tuple
+tuple1 = tuple(sample_list)
+
+print(tuple1)
+print(type(tuple1))
