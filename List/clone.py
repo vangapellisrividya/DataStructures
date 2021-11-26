ba5d214c8964f51fe766a7555858839f58df8568
vangapellisrividya <vangapellisreevidya@gmail.com>
2021-11-26 22:25:06 +0530

    [srividya] Add: copying elemnts in lists

---
+++ List/clone.py
@@ -0,0 +1,13 @@
+''''
+    Created on Nov 26, 2021
+
+    @author:srividya
+    @Date:  26/11/2021
+    @Title: Remove duplicates
+    
+    '''
+
+original_list = [10, 22, 44, 23, 4]
+new_list = list(original_list)
+print(original_list)
+print(new_list)
