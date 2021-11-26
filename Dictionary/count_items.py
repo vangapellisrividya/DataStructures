f11171cd85b608a84379c9569d7efde0d94383de
vangapellisrividya <vangapellisreevidya@gmail.com>
2021-11-26 22:09:30 +0530

    [Srividya] Add: count elements in Dictionary

---
+++ Dictionary/count_items.py
@@ -0,0 +1,11 @@
+'''
+    Created on Nov 25, 2021
+
+    @author:srividya
+    @Date:  25/11/2021
+    @Title: count of items in dictionary
+
+    '''
+
+dict = {'Name': 'pavi', 'Age': 7};
+print ("count : %d" % len (dict))
