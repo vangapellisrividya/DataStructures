31d4f32bbda22308c219c19b6d5b4c18ff6e9018
vangapellisrividya <vangapellisreevidya@gmail.com>
2021-11-26 21:54:48 +0530

    [srividya] Add: array basic functions

---
+++ Arrays/Array.py
@@ -0,0 +1,16 @@
+'''
+    Created on Nov 25, 2021
+
+    @author:srividya
+    @Date:  25/11/2021
+    @Title: Printing array
+
+    '''
+
+from array import *
+class Array:
+    integer = array('i',[5,9,8,7,6])
+    print(integer)
+    for x in integer:
+        print(x)
+   
