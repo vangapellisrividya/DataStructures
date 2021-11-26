30968a66b61c90600c4580eeee5d8038f98bf18b
vangapellisrividya <vangapellisreevidya@gmail.com>
2021-11-26 22:37:25 +0530

    [srividya] Add: basic fun of tuple

---
+++ tuple/tuple.py
@@ -0,0 +1,14 @@
+''''
+    Created on Nov 26, 2021
+
+    @author:srividya
+    @Date:  26/11/2021
+    @Title: Tuple
+
+    '''
+tup = 'python', 'geeks'
+print(tup)
+  
+# Another for doing the same
+tup = ('python', 'geeks')
+print(tup)
