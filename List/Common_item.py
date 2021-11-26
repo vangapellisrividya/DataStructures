b5a9f86adefa712caf175f29211c1f004194c76e
vangapellisrividya <vangapellisreevidya@gmail.com>
2021-11-26 22:20:25 +0530

    [srividya] Add: common items in list

---
+++ List/Common_item.py
@@ -0,0 +1,11 @@
+''''
+    Created on Nov 26, 2021
+
+    @author:srividya
+    @Date:  26/11/2021
+    @Title: Sum list
+    
+    '''
+color1 = "Red", "Green", "Orange", "White"
+color2 = "Black", "Green", "White", "Pink"
+print(set(color1) & set(color2))
