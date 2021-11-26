2ff8472b286971a7570172fa9df0b1ca7298fd40
vangapellisrividya <vangapellisreevidya@gmail.com>
2021-11-26 22:12:51 +0530

    [Srividya] Add: iterating dictionary

---
+++ Dictionary/iterate_dict.py
@@ -0,0 +1,12 @@
+'''
+    Created on Nov 25, 2021
+
+    @author:srividya
+    @Date:  25/11/2021
+    @Title: Iterating dictionary
+
+    '''
+dt = {'a': 'juice', 'b': 'grill', 'c': 'corn'}
+
+for key, value in dt.items():
+    print(key, value)
